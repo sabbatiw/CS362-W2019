acc1f4ae98a00d6b180a76de99b87b04c38f86aa
sabbatiw <sabbatiw@oregonstate.edu>
2019-02-07 10:34:27 -0800

    removed getCost() declaration -> found in dominion_helpers.h

--- projects/sabbatiw/dominion/dominion.h
+++ projects/sabbatiw/dominion/dominion.h
@@ -128,9 +128,6 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
-int getCost(int cardNumber);
-/* returns the value of each card */
-
 void play_smithy(struct gameState *state, int handPos);
 
 #endif
