1fd0b9ea8fda1396462e0a5452ae50ae31f7dc88
sabbatiw <sabbatiw@oregonstate.edu>
2019-02-07 18:41:32 -0800

    refactor to cardtest format

--- projects/sabbatiw/dominion/unittest2.c
+++ projects/sabbatiw/dominion/unittest2.c
@@ -1,73 +1,123 @@
+/*
+ * tests for whoseTurn()
+ */
+
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include "dominion.h"
+#include "dominion_helpers.h"
 
-#define ANSI_RED   "\x1b[31m"
-#define ANSI_GREEN "\x1b[32m"
-#define ANSI_RESET "\x1b[0m"
+#define ANSI_RED    "\x1b[31m"
+#define ANSI_GREEN  "\x1b[32m"
+#define ANSI_YELLOW "\x1b[33m"
+#define ANSI_RESET  "\x1b[0m"
 
-#define NUM_TESTS  4
-#define TEST       "whoseTurn()"
+#define NUM_TESTS   4
+#define TEST        "whoseTurn()"
 
-int dominion_assert(int actual, int expected)
+/*
+ * global counters
+ */
+
+int test_number = 0;
+int passed_tests = 0;
+
+/*
+ * function: dom_assert
+ * --------------------
+ * determines if two values are equal and displays the results
+ */
+
+void dom_assert(int actual, int expected, char *description)
 {
-    return actual == expected;
+    printf("[ ");
+
+    if (actual == expected)
+    {
+        passed_tests++;
+        printf(ANSI_GREEN "passed" ANSI_RESET);
+    }
+
+    else
+    {
+        printf(ANSI_RED "failed" ANSI_RESET);
+    }
+  
+    printf(" ] test %d (%s): expected = %d, actual = %d\n",  
+           ++test_number, description, expected, actual);
 }
+
+/*
+ * function: print_line
+ * --------------------
+ * prints a dashed line of 80 chars
+ */
+
+void print_line()
+{
+    printf("---------------------------------------" \
+           "---------------------------------------\n");
+}
+
+/*
+ * function: test_whoseTurn
+ * ------------------------------
+ * numHandCards returns the number of cards in a player's hand
+ */
 
 void test_whoseTurn()
 {
-    int actual[NUM_TESTS];
-    int count;
-    int expected[NUM_TESTS];
-    int i;
+    struct gameState Test;
 
-    struct gameState input[NUM_TESTS];
+    int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, 
+                 sea_hag, tribute, smithy};
+
+    /* prepare pre and post game states */
+    initializeGame(4, k, rand() + 1, &Test);
+
+    print_line();
+    printf(ANSI_YELLOW "begin %s testing\n" ANSI_RESET, TEST);
+    print_line();
 
     /* test 1 */
-    input[0].whoseTurn = 0;
-    expected[0] = 0;
+    dom_assert(whoseTurn(&Test), 0, "player 1");
 
     /* test 2 */
-    input[1].whoseTurn = 19;
-    expected[1] = 19;
+    Test.whoseTurn = 1;
+    dom_assert(whoseTurn(&Test), 1, "player 2");
 
     /* test 3 */
-    input[2].whoseTurn = INT_MAX;
-    expected[2] = INT_MAX;
+    Test.whoseTurn = 2;
+    dom_assert(whoseTurn(&Test), Test.whoseTurn, "player 3");
 
-    /* test 4 */
-    input[3].whoseTurn = INT_MIN;
-    expected[3] = INT_MIN;
+    /* tests 4*/
+    Test.whoseTurn = 3;
+    dom_assert(whoseTurn(&Test), Test.whoseTurn, "player 4");
+       
+    print_line();
 
-    printf("----------------------------\n");
-
-    count = 0;
-
-    for (i = 0; i < NUM_TESTS; i++)
-    {
-        printf("%s test %d: [", TEST, i + 1);
-        actual[i] = whoseTurn(&input[i]);
-        if (dominion_assert(actual[i], expected[i]))
-        {
-            count++;
-            printf(ANSI_GREEN "passed" ANSI_RESET "]\n");
-        }
-        
-        else
-        {
-            printf(ANSI_RED "failed" ANSI_RESET "] expected value: ");
-            printf("%d, actual value: %d\n", expected[i], actual[i]);
-        }
-    }
-    printf("----------------------------\n");
-    printf("%d of %d tests completed.\n", i, NUM_TESTS);
-    printf("%d of %d tests successful.\n", count, NUM_TESTS);
-    printf("----------------------------\n");
-
+    /* totals */
+    printf("%s tests: %d of %d completed.\n", TEST, test_number, NUM_TESTS);
+    printf("                   %d of %d successful.\n",
+            passed_tests, NUM_TESTS);
+    print_line();
 }
+
+/*
+ * function: main
+ * --------------
+ * seed rng and run the tests
+ */
 
 int main(void)
 {
+    time_t t;
+
+    srand((unsigned) time(&t));
     test_whoseTurn();
+    
     return 0;
 }
