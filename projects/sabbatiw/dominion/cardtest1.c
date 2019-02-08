2074f28fd50cdb86271468e849551c3a781b1c3d
sabbatiw <sabbatiw@oregonstate.edu>
2019-02-07 17:09:32 -0800

    add comments

--- projects/sabbatiw/dominion/cardtest1.c
+++ projects/sabbatiw/dominion/cardtest1.c
@@ -1,3 +1,7 @@
+/*
+ * tests for smithy card effects
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,8 +16,18 @@
 #define NUM_TESTS  9
 #define TEST       "smithy"
 
+/*
+ * global counters
+ */
+
 int test_number = 0;
 int passed_tests = 0;
+
+/*
+ * function: dom_assert
+ * --------------------
+ * determines if two values are equal and displays the results
+ */
 
 void dom_assert(int actual, int expected, char *description)
 {
@@ -34,11 +48,31 @@ void dom_assert(int actual, int expected, char *description)
            ++test_number, description, expected, actual);
 }
 
+/*
+ * function: print_line
+ * --------------------
+ * prints a dashed line of 80 chars
+ */
+
 void print_line()
 {
     printf("---------------------------------------" \
            "---------------------------------------\n");
 }
+
+/*
+ * function: test_smithy_card
+ * ------------------------------
+ * test 1) p1 hand size
+ * test 2) p1 deck size
+ * test 3) p1 discards
+ * test 4) p2 hand size
+ * test 5) p2 deck size
+ * test 6) p2 discards
+ * test 7) number of cards played
+ * test 8) victory card pile
+ * test 9) kingdom card pile
+ */
 
 void test_smithy_card()
 {
@@ -48,28 +82,40 @@ void test_smithy_card()
     int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, 
                  sea_hag, tribute, smithy};
 
+    /* prepare pre and post game states */
     initializeGame(2, k, rand() + 1, &Pre);
     memcpy(&Post, &Pre, sizeof Post);
+
+    /* play smithy */
     cardEffect(smithy, 0, 0, 0, &Post, 0, 0);
     
     print_line();
 
+    /* test 1: we increase hand size +(3 - 1) */
     dom_assert(Post.handCount[0], Pre.handCount[0] + 2, "p1 hand size");
+
+    /* test 2: we decrease deck size -3 */
     dom_assert(Post.deckCount[0], Pre.deckCount[0] - 3, "p1 deck size");
+
+    /* test 3: we don't discard any */
     dom_assert(Post.discardCount[0], Pre.discardCount[0], "p1 discards");
 
+    /* tests 4-6: no state change for player 2 */
     dom_assert(Post.handCount[1], Pre.handCount[1], "p2 hand size");
     dom_assert(Post.deckCount[1], Pre.deckCount[1], "p2 deck size");
     dom_assert(Post.discardCount[1], Pre.discardCount[1], "p2 discards");
 
+    /* test 7: we increase hand size +2 */
     dom_assert(Post.playedCardCount, Pre.playedCardCount + 1, 
             "cards played");
 
+    /* test 8: no change in victory card pile */
     dom_assert(Post.supplyCount[estate] + Post.supplyCount[duchy] + 
                Post.supplyCount[province], Pre.supplyCount[estate] + 
                Pre.supplyCount[duchy] + Pre.supplyCount[province], 
                "victory cards");
 
+    /* test 9: no change in kingdom card pile */
     dom_assert(Post.supplyCount[k[0]] + Post.supplyCount[k[1]] + 
                Post.supplyCount[k[2]] + Post.supplyCount[k[3]] +
                Post.supplyCount[k[4]] + Post.supplyCount[k[5]] +
@@ -80,13 +126,21 @@ void test_smithy_card()
                Pre.supplyCount[k[4]] + Pre.supplyCount[k[5]] +
                Pre.supplyCount[k[6]] + Pre.supplyCount[k[7]] +
                Pre.supplyCount[k[8]] + Pre.supplyCount[k[9]], "kingdom cards");
-    
+        
     print_line();
+
+    /* totals */
     printf("%s tests: %d of %d completed.\n", TEST, test_number, NUM_TESTS);
     printf("              %d of %d successful.\n",
             passed_tests, NUM_TESTS);
     print_line();
 }
+
+/*
+ * function: main
+ * --------------
+ * seed rng and run the tests
+ */
 
 int main(void)
 {
@@ -94,5 +148,6 @@ int main(void)
 
     srand((unsigned) time(&t));
     test_smithy_card();
+    
     return 0;
 }
